fb6f2ffa4f4f288ed539d9bb29ec822c0b23e6b7
Alex <4493505+AlexFuentes117@users.noreply.github.com>
2023-09-18 12:55:07 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -76,7 +76,6 @@ resource "aws_security_group" "SG_Public_Instance" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
